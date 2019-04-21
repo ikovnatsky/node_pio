02b9564ac1691b5c4c02be8d48090d2aa671337a
ikovnatsky <ilya@ptgroupinc.com>
2019-04-21 05:20:44 -0700

    added support for multi epoch super epoch

--- src/SMNode.h
+++ src/SMNode.h
@@ -2,10 +2,17 @@
 #include "SMCommon.h"
 #include "list"
 
+#include "STMsgTag.h"
+
 #define MODE_NORMAL 0
 #define MODE_DEBUG  1
 #define ROLE_NORMAL 0
 #define ROLE_BEACON 1
+
+
+#define CHARGE_STATE_NONE 0
+#define CHARGE_STATE_CHARGING 1
+#define CHARGE_STATE_FULL  2
 typedef struct
 {
   float lon,lat,z;
@@ -35,8 +42,10 @@ class SMNode
     bool bActive;
     static unsigned char MacId[6];
     short nConnectionId;
-    short nTimeslot;
-    char nChannel;
+    unsigned short nTimeslot;
+    char  nChannel;
+    unsigned short nTimeslotMask;
+    uint8_t maxTimeSlotLen;
     unsigned char nGatewayId;
 
     int FindBeaconSendJoin();
@@ -45,11 +54,12 @@ class SMNode
     int ProcessTimestampMsg(unsigned char *p,int len);
     int ProcessDownMessage(unsigned char *p,int len);
     int ProcessGrantMsg(unsigned char *p,int len);
-
+    int ParseDownMessage(uint8_t *p, int len);
     int BuildJoinMessage(LORA_PKT *pkt, int gatewayId, int rssi);
     int BuildUpstreamMessage(LORA_PKT *pkt, int nRSSI, unsigned char *payload, int len);
+    int BuildUpstreamMessage(LORA_PKT *pkt, int nRSSI, int max_len);
 
-    int rxEpoch;
+    uint8_t rxEpoch;
     int rxGatewayID;
     int rxRSSI;
     unsigned char otaMode;
@@ -70,17 +80,37 @@ class SMNode
     uint8_t ledPWM;
     std::list<RANGE_RESULT> RangeResults;
     
+    std::list<STMsgTag> upstreamQ;
+
     char line1[30];
     char line2[30];
     
     int8_t zoneOffset;
     // for Now
-    unsigned char rxData[30];
+    char alertText[30];
+    unsigned char alertLevel;
     unsigned char rxDataLen;
     unsigned char beaconReceived; 
     unsigned char  stop;
     unsigned char  mode;
     unsigned char  role;
+    unsigned char  panic;
+    unsigned char configChanged;
+    uint8_t soc;  // state of charge
+    uint8_t charge_state;
+    float    avgI;
+    float press;
+    float ttE;
+
+    int loopCnt;
+    int opModeSpot;
+    int last_send_time;
+
+    // Send timers
+    TagSendTimer socTimer,motTimer,pressTimer;
+
+    String fwServer;
+    String fwImage;
 };
 
 
