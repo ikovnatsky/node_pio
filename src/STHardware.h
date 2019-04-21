3fe7e85073d94a87aa94b31748c87a51b4b278c9
ikovnatsky <ilya@ptgroupinc.com>
2019-04-21 05:19:53 -0700

    Added support for new hardware

--- src/STHardware.h
+++ src/STHardware.h
@@ -1,3 +1,5 @@
+#define V2
+
 #define LORA_SS   13
 #define LORA_TXEN 26
 #define LORA_RXEN 27
@@ -13,15 +15,34 @@
 #define GPS_TX_PIN 21
 
 #define CS_DISPLAY 12
-#define BIT_RESETn  4
-#define BIT_BUSY    2
-#define BIT_MTR    0x20
-#define BIT_DISCHARGE  1
 #define EXPANDER_IO_PIN_OFFSET 0x40
 #define PIN_DISCHARGE   EXPANDER_IO_PIN_OFFSET+0
-#define PIN_MTR  EXPANDER_IO_PIN_OFFSET + 5
 #define PIN_BUTTONS     35
 
 #define PIN_AUDIO_SD    22
 
+#define CS_1 12
+#define CS_2 13
+#define CS_3 14
+#define CS_4 18
 
+
+#ifdef V1
+#define PIN_MTR  EXPANDER_IO_PIN_OFFSET + 5
+#define BIT_RESETn  4
+#define BIT_BUSY    2
+#define BIT_MTR    0x20
+#define BIT_DISCHARGE  1
+#else
+#define BIT_MTR    0x4000
+#define BIT_SHUTDOWN 0x1000
+#define BIT_RESETn  4
+#define BIT_BUSY    2
+
+#define PIN_MTR  EXPANDER_IO_PIN_OFFSET + 14
+#define PIN_BUTTON_1 EXPANDER_IO_PIN_OFFSET+8
+#define PIN_BUTTON_2 EXPANDER_IO_PIN_OFFSET+9
+#define PIN_BUTTON_3 EXPANDER_IO_PIN_OFFSET+10
+#define PIN_BUTTON_4 EXPANDER_IO_PIN_OFFSET+15
+#define PIN_SHUTDOWN  EXPANDER_IO_PIN_OFFSET+12
+#endif
